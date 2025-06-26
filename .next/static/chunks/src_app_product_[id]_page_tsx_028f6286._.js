01d023374c1658f2fc2b4463166af6684b0ef327
Jaxxy <anisdellalitravel@gmail.com>
2025-06-26 23:40:57 +0200

    Add .next/static/chunks/src_app_product_[id]_page_tsx_028f6286._.js 1750974056320

---
+++ .next/static/chunks/src_app_product_[id]_page_tsx_028f6286._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_30e862d7._.js",
+  "static/chunks/src_1d972301._.js"
+],
+    source: "dynamic"
+});
